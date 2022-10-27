73112aef73e252474b3c9b0e10912f1f4d42592d
Alexander <alexanderjoemc@gmail.com>
2022-10-27 00:25:41 -0400

    Files

---
+++ src/chromosome.rs
@@ -0,0 +1,73 @@
+pub struct Chromosome {
+    pub genes: Vec<u32>,
+    pub fitness: i32,
+}
+
+impl Chromosome {
+    // create a getter
+    pub fn get_genes(&self) -> Vec<u32> {
+        self.genes.clone()
+    }
+    // create a setter
+    pub fn set_genes(&mut self, genes: Vec<u32>) {
+        self.genes = genes;
+    }
+
+    // set a single gene
+    pub fn set_gene(&mut self, index: usize, value: u32) {
+        self.genes[index] = value;
+    }
+
+    // get single gene
+    pub fn get_gene(&self, index: usize) -> u32 {
+        self.genes[index]
+    }
+
+    pub fn get_fitness(&self) -> i32 {
+        self.fitness
+    }
+
+    pub fn set_fitness(&mut self, fitness: i32) {
+        self.fitness = fitness;
+    }
+
+    pub(crate) fn new() -> Chromosome {
+        Chromosome {
+            genes: Vec::new(),
+            fitness: 0,
+        }
+    }
+
+    // calculate fitness
+    pub fn calculate_fitness(&mut self) {
+        let mut fitness = 0;
+        for i in 0..self.genes.len() {
+            fitness += self.genes[i] as i32;
+        }
+        self.fitness = fitness;
+    }
+
+    // return string representation of chromosome
+    pub fn to_string(&self) -> String {
+        let mut s = String::new();
+        for i in 0..self.genes.len() {
+            s.push(self.genes[i].to_string().chars().next().unwrap());
+        }
+        s
+    }
+
+    // clone
+    pub fn clone(&self) -> Chromosome {
+        let mut c = Chromosome::new();
+        c.set_genes(self.genes.clone());
+        c.set_fitness(self.fitness.clone());
+        c
+    }
+}
+
+// clone trait
+impl Clone for Chromosome {
+    fn clone(&self) -> Chromosome {
+        self.clone()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,150 @@
+mod chromosome;
+
+fn main() {
+    // an array of chromosomes of size 100
+    let mut population: Vec<chromosome::Chromosome> = Vec::new();
+    let mut new_population: Vec<chromosome::Chromosome> = Vec::new();
+    // desired fitness int 20
+    let desired_fitness = 20;
+    // population size
+    let pop_size = 100;
+    // percent crossover
+    let crossover_rate = 0.5;
+    // percent mutation
+    let mutation_rate = 0.01;
+    // current generation
+    let mut generation = 0;
+
+    println!("Initializing population...");
+    population = initialize_population(pop_size);
+
+    // print each chromosome in the population
+    // for i in 0..population.len() {
+    //     println!("{}: {}", i, population[i].to_string());
+    // }
+
+    let mut best_fitness = 0 as i32;
+    let mut best_chromosome = chromosome::Chromosome::new();
+
+    println!("Population initialized. Starting evolution...");
+    // loop until we find a chromosome with fitness of 20
+    while best_fitness < desired_fitness {
+        while new_population.len()  < pop_size as usize {
+            // select two parents
+            let mut parent1 = select_parent(&population, pop_size);
+            let mut parent2 = select_parent(&population, pop_size);
+
+            // random float between 0 and 1
+            let mut random = rand::random::<f64>();
+            if random < crossover_rate {
+                crossover(&mut parent1, &mut parent2);
+            }
+
+            // random float between 0 and 1
+            random = rand::random::<f64>();
+            if random < mutation_rate {
+                mutate(&mut parent1);
+                mutate(&mut parent2);
+            }
+
+            // add parents to new population
+            new_population.push(parent1);
+            new_population.push(parent2);
+        }
+        // replace old population with new population
+        population.clear();
+        population = new_population.to_vec();
+        new_population.clear();
+
+        let best = get_most_fit(&mut population);
+        // print best
+        println!("Generation {}: {}", generation, best.to_string());
+        println!("Generation: {}, Best Fitness: {}", generation, best.get_fitness());
+        generation += 1;
+        best_fitness = best.get_fitness();
+    }
+
+
+    // loop until we find the desired fitness
+}
+
+// initialize population function
+fn initialize_population(pop_size: usize) -> Vec<chromosome::Chromosome> {
+    let mut population: Vec<chromosome::Chromosome> = Vec::new();
+    for _ in 0..pop_size {
+        let mut cr = chromosome::Chromosome::new();
+        // temp array of 20 random genes
+        let mut genes: Vec<u32> = Vec::new();
+        for _ in 0..20 {
+            // random int 0-1
+            let r = rand::random::<u32>() % 2;
+            genes.push(r);
+        }
+        cr.set_genes(genes);
+        cr.calculate_fitness();
+        population.push(cr);
+    }
+    population
+}
+
+fn select_parent(population: &Vec<chromosome::Chromosome>, pop_size: usize) -> chromosome::Chromosome {
+    // use tournament selection
+    let mut tournament: Vec<chromosome::Chromosome> = Vec::new();
+    // add random chromosomes to the tournament
+    for _ in 0..5 {
+        // random int between 0 and pop_size
+        let r = rand::random::<usize>() % pop_size;
+        tournament.push(population[r].clone());
+    }
+    // sort the tournament by fitness
+    tournament.sort_by(|a, b| b.fitness.cmp(&a.fitness));
+    // return the best chromosome
+    tournament[0].clone()
+}
+
+// crossover return both parents
+fn crossover(parent1: &mut chromosome::Chromosome, parent2: &mut chromosome::Chromosome) {
+    // random int between 0 and 19
+    let r = rand::random::<usize>() % 20;
+    let mut parent1_new: Vec<u32> = Vec::new();
+    let mut parent2_new: Vec<u32> = Vec::new();
+    // take parent1 genes up to r and parent2 genes after r
+    for i in 0..20 {
+        if i < r {
+            parent1_new.push(parent1.genes[i]);
+            parent2_new.push(parent2.genes[i]);
+        } else {
+            parent1_new.push(parent2.genes[i]);
+            parent2_new.push(parent1.genes[i]);
+        }
+    }
+    parent1.set_genes(parent1_new);
+    parent2.set_genes(parent2_new);
+}
+
+// mutation
+fn mutate(chromosome: &mut chromosome::Chromosome) {
+    // random int between 0 and 19
+    let r = rand::random::<usize>() % 20;
+    // flip the gene
+    if chromosome.get_gene(r) == 0 {
+        chromosome.set_gene(r, 1);
+    } else {
+        chromosome.set_gene(r, 0);
+    }
+}
+
+
+// get populations best fitness
+fn get_most_fit(population: &mut Vec<chromosome::Chromosome>) -> chromosome::Chromosome {
+    let mut best_fitness = 0;
+    let mut best_chromosome = chromosome::Chromosome::new();
+    for i in 0..population.len() {
+        population[i].calculate_fitness();
+        if population[i].get_fitness() > best_fitness {
+            best_fitness = population[i].get_fitness();
+            best_chromosome = population[i].clone();
+        }
+    }
+    best_chromosome
+}
